7a2163ee63a5182e77ab43095284c56a7fdfcc43
UX Lab <UXLab@EIAS208D18052.local>
2018-03-16 16:46:12 -0600

    Map

---
+++ Maps/app/src/main/java/mx/itesm/maps/MapsActivity.java
@@ -0,0 +1,85 @@
+package mx.itesm.maps;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in salon and move the camera
+        LatLng salon = new LatLng(20.737125, -103.454248);
+        mMap.addMarker(new MarkerOptions().position(salon).title("El salon"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(salon, 15));
+        mMap.setOnMapClickListener(this);
+        setMyLocation();
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        Log.d("MAP", latLng.toString());
+        mMap.addMarker(new MarkerOptions().position(latLng).title("CLICKEADO EN EL MAPA").alpha(0.5f).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+    }
+
+    public void setMyLocation() {
+
+        // Verificar que haya permiso y si hay habilitar capa
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // Pedir permiso
+            // arreglo de strings que son los permisos
+            // request code - igual que con las actividades
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        } else {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if(requestCode == 0 && results[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.wtf("PERMISOS", "SI AUTORIZO");
+        } else {
+            Log.wtf("PERMISOS", "NO HAY AUTORIZACION");
+        }
+    }
+
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# TC2024 - Mobile Applications Development Course
-
-## Projects developed in the TC2024 Course
-
-> Feel free to review the topics.
